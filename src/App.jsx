4a15cda4f8af0d65acecf1db9ab6dbe54a3a91a6
Vanita <vanitabarhman9728@gmail.com>
2024-10-15 18:58:34 +0530

    Topic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-// import ShowOffer from "./components/ShowOffer"
+import ShowOffer from "./components/ShowOffer"
 
 import Test from "./components/Test"
-// import Todolist from "./components/Todolist"
+import Todolist from "./components/Todolist"
 
-// import UndoRedu from './components/UndoRedu'
-//   import Traffic from './components/Traffic'
-  // import List from './components/List'
+import UndoRedu from './components/UndoRedu'
+  import Traffic from './components/Traffic'
+  import List from './components/List'
   function App() {
   return (
     <div>
 
-{/*   
+  
     <Traffic/>
     <ShowOffer/>
-    <UndoRedu/> */}
-    {/* <List/> */}
-   {/* <Todolist/> */}
+    <UndoRedu/>
+    <List/>
+   <Todolist/>
    <Test/> 
     </div>
   )
